2a663601c8ce50d87e6bef030e2d32a363bb6fde
Zixi Qiao <qiaozixi@gmail.com>
2024-07-17 14:56:26 -0400

    initialized repo

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <Adafruit_BME680.h>
+#include <AdafruitIO_Feed.h>
+#include <Adafruit_GPS.h>
+#include <Adafruit_ST7789.h>
+#include <Adafruit_GFX.h>
+#include <config.hpp>
+
+void setup() {
+
+}
+
+void loop() {
+
+}
