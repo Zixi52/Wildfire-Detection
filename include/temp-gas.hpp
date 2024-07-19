1056610f3550abee2f6f4e41f5276fd26984c0a7
Zixi52 <117396225+Zixi52@users.noreply.github.com>
2024-07-19 11:23:08 -0400

    add(temp-gas.hpp): edited temp-gas.hpp (#3) (#5)
    add: function temperature_measurement and voc_measurement
    
    Co-authored-by: Bruce-Huang <149635578+BruceHuang214@users.noreply.github.com>
    Co-authored-by: Tianyou Huang <tianyou.huang@hotmail.com>

--- include/temp-gas.hpp
+++ include/temp-gas.hpp
@@ -1,11 +1,20 @@
-#ifndef GAS_HPP
-#define GAS_HPP
+#ifndef TEMPGAS_HPP
+#define TEMPGAS_HPP
 
 #include <Adafruit_SGP40.h>
 #include <Adafruit_BME680.h>
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
 
-namespace GAS {
+namespace TEMPGAS {
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+#define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_SGP40 sgp(*theWire);
 Adafruit_BME680 bme;
@@ -28,25 +37,41 @@ void initialize() {
     Serial.print(sgp.serialnumber[0], HEX);
     Serial.print(sgp.serialnumber[1], HEX);
     Serial.println(sgp.serialnumber[2], HEX);
+
+    // Set up oversampling and filter initialization
+    bme.setTemperatureOversampling(BME680_OS_8X);
+    bme.setHumidityOversampling(BME680_OS_2X);
+    bme.setPressureOversampling(BME680_OS_4X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    bme.setGasHeater(320, 150); // 320*C for 150 ms
+    // Tell BME680 to begin measurement.
+    unsigned long endTime = bme.beginReading();
+    if (endTime == 0) {
+        Serial.println(F("Failed to begin reading :("));
+        return;
+    }
 }
 
-void measurement() {
-    uint16_t sraw;
-    int32_t voc_index;
-  
-    float t = bme.readTemperature();
+double temperature_measurement() {
+    double t = bme.readTemperature();
     Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
-    float h = bme.readHumidity();
+    double h = bme.readHumidity();
     Serial.print("Hum. % = "); Serial.println(h);
 
-    sraw = sgp.measureRaw(t, h);
-    Serial.print("Raw measurement: ");
-    Serial.println(sraw);
+    return t;
+}
 
+int32_t voc_measurement(){
+    int32_t voc_index;
+    double t = bme.readTemperature();
+    double h = bme.readHumidity();
     voc_index = sgp.measureVocIndex(t, h);
     Serial.print("Voc Index: ");
     Serial.println(voc_index);
+
+    return voc_index;
 }
-}
+
+}//namespace TEMPGAS
 
 #endif
